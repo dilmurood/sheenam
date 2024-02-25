f801de77df95c04a05d8cae5ac0d05e29ca2dadc
Dilmurod Rozikov <2110115@newuzbekistanuniversity.uz>
2024-02-25 15:37:14 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogInAsync -> WIP

---
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,17 @@
+﻿using Sheenam.Api.Broker.Storages;
+using Sheenam.Api.Models.Foundations;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IStorageBroker _storageBroker;
+        public GuestService(IStorageBroker storageBroker) =>
+            this._storageBroker = storageBroker;
+        public ValueTask<Guest> AddGuestAsync(Guest guest)
+        {
+            return  _storageBroker.InsertGuestAsync(guest);
+        }
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Guests/IGuestService.cs
@@ -0,0 +1,10 @@
+﻿using Sheenam.Api.Models.Foundations;
+using System.Threading.Tasks;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public interface IGuestService
+    {
+        ValueTask<Guest> AddGuestAsync(Guest guest);
+    }
+}
