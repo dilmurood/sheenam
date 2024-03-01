285123d5f00625034cdc42c836833521332a7bc5
Dilmurod Rozikov <2110115@newuzbekistanuniversity.uz>
2024-03-01 15:40:59 +0500

    CODE RUB: Upgrade SameExcpetionAs Method

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -55,12 +55,9 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
         private static int GetRandomNumber() =>
             new IntRange(2, 9).GetValue();
 
-        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException)
-        {
-            return actualException => actualException.Message == expectedException.Message
-                && actualException.InnerException.Message == expectedException.InnerException.Message
-                && (actualException.InnerException as Xeption).DataEquals(expectedException.InnerException.Data);
-        }
+        private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
+            actualException => actualException.SameExceptionAs(expectedException);
+        
         private static Filler<Guest> CreateGuestFiller(DateTimeOffset date)
         {
             var filler = new Filler<Guest>();
